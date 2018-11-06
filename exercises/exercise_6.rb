18f5f16e207645292ffb904892a6a325549d7c17
carlocadiz <carlocadiz71@gmail.com>
2018-11-06 19:01:43 +0000

    completed exercise 6 and add gem faker

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -1,3 +1,4 @@
+require 'faker'
 require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
@@ -9,3 +10,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+5.times do
+  @store1.employees.create! first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, hourly_rate: Faker::Number.decimal(2)
+end
+
+7.times do
+  @store2.employees.create! first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, hourly_rate: Faker::Number.decimal(2)
+end
+
